ab96381e77019adc284ba597b1817e08e429be9f
okormorupraise <okormorupraisecoding@gmail.com>
2023-04-25 08:41:06 -1200

    newone

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./Header"
+import Landingpage from "./Landingpage"
 export default function Home() {
 
   return (
     <div className="relative overflow-x-hidden">
 <Header/>
+<Landingpage/>
     </div>
   )
 }
