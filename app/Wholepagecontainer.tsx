169663917c3cfb6b6596b2b603a18182e9a67cff
okormorupraise <okormorupraisecoding@gmail.com>
2023-04-24 14:18:22 -1200

    newone

--- app/Wholepagecontainer.tsx
+++ app/Wholepagecontainer.tsx
@@ -2,8 +2,7 @@ import React, { Suspense } from 'react'
 import Header from './Header'
 import Landingpage from './Landingpage'
 import Whyenver from './Whyenver'
-import Services from './Services'
-import Amazingportfolio from './Amazingportfolio'
+
 import Contactus from './Contactuse'
 import Footer from './Footer'
 import Introduction from './Introduction'
@@ -12,23 +11,19 @@ function Wholepagecontainer() {
   return (
     <div>
        <header className='w-full z-30 block  overflow-hidden'>
-    <Suspense fallback="loading">
-       
+   
       <Header/>
    
-      </Suspense>
        </header>
     <main>
-    <Suspense fallback="loading">
-       
+   
     <Landingpage/>
     
-       </Suspense>
+    
       
      
       <Whyenver/>
-      <Services/>
-      <Amazingportfolio/>
+  
       <Contactus/>
     </main>
     
